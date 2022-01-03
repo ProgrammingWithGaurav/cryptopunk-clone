e3bdcc78b37b33ba4180161b71e943f3c88c7095
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-01-03 13:53:44 +0530

    Adding  PunkLists Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
 import './App.css';
+import CollectionCard from './components/CollectionCard/CollectionCard';
+import axios from 'axios';
+import Punklist from './components/Punklist/Punklist';
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x965c9e7362Fe55f71cea872C0D0ff78606a44bDF&order_direction=asc');
+
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
   return (
     <div className="app">
       <Header />
+      <Punklist punkListData={punkListData} />
     </div>
   )
 }
